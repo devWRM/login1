1bb3d2da60bf41ea6d785f04453521d9aad00f42
devWRM <mcwen@comcast.net>
2021-05-08 21:33:48 -0400

    created Login functional component with input for Username & Password

---
+++ src/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Login() {
+
+    return(
+        <div>
+            <h3>Login page</h3>
+            <label>Username</label><sp> </sp>
+            <input></input>
+
+            <br /><br />
+
+            <label>Password</label><sp> </sp>
+            <input></input>
+            
+        </div>
+    )
+}
+
+export default Login;
